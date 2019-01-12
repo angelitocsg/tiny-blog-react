bb372526bccf892a2d58345517321856958b85e5
Angelito Casagrande <angelito@angelito.com.br>
2019-01-12 10:37:06 -0200

    Incluído bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './assets/css/bootstrap.scss'
+import './assets/js/bootstrap'
+
 ReactDOM.render(
-    <h1>Tiny Blog</h1>,
+    <h1>Tiny Blog</h1>, 
     document.getElementById('root')
 )
